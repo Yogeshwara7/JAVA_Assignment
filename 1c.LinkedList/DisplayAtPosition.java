a9f942f8206c4e17984bf78cccbc57eb5bbf5ae0
Yogeshwara B <yogeshwara567@gmail.com>
2025-05-20 19:36:40 +0530

    code

---
+++ 1c.LinkedList/DisplayAtPosition.java
@@ -0,0 +1,18 @@
+/*Write a Java program to display elements and their positions in a linked list ( using
+l_listobj.get(p) )*/
+
+
+import java.util.LinkedList;
+
+public class DisplayAtPosition {
+    public static void main(String[] args) {
+        LinkedList<String> colors = new LinkedList<>();
+        colors.add("Red");
+        colors.add("Yellow");
+        colors.add("White");
+
+        for (int i = 0; i < colors.size(); i++) {
+            System.out.println("Position " + i + ": " + colors.get(i));
+        }
+    }
+}
