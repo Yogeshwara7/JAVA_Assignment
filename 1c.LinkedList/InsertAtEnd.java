e4ed7d8d877f086f9cea5391c379a419ef9d1bdf
Yogeshwara B <yogeshwara567@gmail.com>
2025-05-20 19:30:16 +0530

    Insert Specified Element at the End

---
+++ 1c.LinkedList/InsertAtEnd.java
@@ -0,0 +1,17 @@
+/*Write a Java program to insert the specified element at the end of a linked list.( using
+l_listobj.offerLast("Pink"))*/
+
+
+import java.util.LinkedList;
+
+public class InsertAtEnd {
+    public static void main(String[] args) {
+        LinkedList<String> colors = new LinkedList<>();
+        colors.add("Red");
+        colors.add("Orange");
+
+        colors.offerLast("Pink");
+
+        System.out.println("After inserting at end: " + colors);
+    }
+}
