5ef468959500687ec73cffab85c38d7dacfb897a
Yogeshwara B <yogeshwara567@gmail.com>
2025-05-21 19:22:54 +0530

    stringreverse

---
+++ 3b.StringExercise/StringReverser.java
@@ -0,0 +1,26 @@
+/*
+ * Write a Java Program for Reversing the characters in a string using user defined function
+reverseString().
+ */
+
+
+public class StringReverser {
+
+    // User-defined function to reverse a string
+    public static String reverseText(String input) {
+        if (input == null) return null;
+        return new StringBuilder(input).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+        // Original string
+        String originalText = "Java Programming";
+
+        // Call the reverseText() function
+        String reversedText = reverseText(originalText);
+
+        // Output
+        System.out.println("Original String: " + originalText);
+        System.out.println("Reversed String: " + reversedText);
+    }
+}
